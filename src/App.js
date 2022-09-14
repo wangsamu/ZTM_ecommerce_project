9e915729e7d7fae8f6ce39f4d926bf113b9fdba2
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-14 15:40:53 +0200

    navbar styles created, sign-in page route and scaffolding done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./routes/home/Home";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/Navigation";
 import Shop from "./routes/shop/Shop";
+import SignIn from "./routes/sign-in/SignIn";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
