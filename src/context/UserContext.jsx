4ec882280fb1ab4e5c5c26d66ae8ef710efde314
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-16 10:16:01 +0200

    naming errors fixed, UserContext and UserProvider components working correctly

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -4,14 +4,11 @@ import { createContext, useState } from "react";
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
-
 });
 
-export const userProvider = (({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null)
-  const value = {currentUser, setCurrentUser}
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser, setCurrentUser };
 
-  return (<UserContext.Provider value={}>
-    {children}
-  </UserContext.Provider>)
-})
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
